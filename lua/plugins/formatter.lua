9a9b8fcd2641d1ff96e07a46623f58402489a8a7
MrPelmenis <misterpelmenis@gmail.com>
2026-07-10 12:59:22 +0300

    formatter++

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -16,6 +16,8 @@ return {
         -- Define formatters per language
         formatters_by_ft = {
             lua = { "stylua" },
+            c = { "clang-format" },
+            cpp = { "clang-format" },
             python = { "black" },
             javascript = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
@@ -23,6 +25,22 @@ return {
             css = { "prettierd", "prettier", stop_after_first = true },
             rust = { "rustfmt" },
         },
+
+        formatters = {
+            ["clang-format"] = {
+                prepend_args = {
+                    "--style={"
+                    .. "BasedOnStyle: LLVM, "
+                    .. "IndentWidth: 4, "
+                    .. "TabWidth: 4, "
+                    .. "UseTab: Never, "
+                    .. "BinPackParameters: false, " -- Forces one parameter per line
+                    .. "AllowAllParametersOfDeclarationOnNextLine: false"
+                    .. "}",
+                },
+            },
+        },
+
         -- Optional: Setup format-on-save automatically
         format_on_save = {
             timeout_ms = 500,
